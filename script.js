8552a77707f76850ecec978ff369e2d152ac23c7
egalland <emmanuel.galland117@gmail.com>
2018-06-25 14:20:21 +0200

    Correction exercice 6

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-// FONCTION CHANGEMENT D'IMAGE
-function swapImage(id, image){
-  document.getElementById(id).src = image;
+// FONCTION CHANGEMENT D'IMAGE LORSQU'ON RENTRE DANS LA ZONE DE L'IMAGE
+function swapImage(id){
+    // CHANGEMENT DE LA SOURCE DE L'IMAGE
+    document.getElementById(id).src = 'assets/images/' + id + '_2.jpg';
+}
+// FONCTION CHANGEMENT D'IMAGE LORSQU'ON SORT DE LA ZONE DE L'IMAGE
+function reverseImage(id){
+    // CHANGEMENT DE LA SOURCE DE L'IMAGE
+    document.getElementById(id).src = 'assets/images/' + id + '.jpg';
 }
